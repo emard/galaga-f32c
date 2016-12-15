91a41709c864a090b16d009d54ea17d426819db0
davor <davor@ssd.lan>
2016-12-15 14:02:09 +0100

    shapes for bomb, missile and double-ship

--- shapes.h
+++ shapes.h
@@ -15,7 +15,7 @@ struct charcolors std_colors[] =
   {0, 0}
 };
 
-char *shape_ship[] =
+char *shape_ship1[] =
 {
 "    WWWW",
 "    WWWW",
@@ -33,6 +33,27 @@ char *shape_ship[] =
 "WWWW    WWWW",
 "WWWW    WWWW",
 "WWWW    WWWW",
+NULL
+};
+
+char *shape_ship2[] =
+{
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"    WWWW    WWWW",
+"WWWW    WWWW    WWWW",
+"WWWW    WWWW    WWWW",
+"WWWW    WWWW    WWWW",
+"WWWW    WWWW    WWWW",
 NULL
 };
 
@@ -407,27 +428,52 @@ NULL
 };
 
 
+char *shape_bomb[] =
+{
+"WWWW",
+"WWWW",
+"WWWW",
+"WWWW",
+NULL
+};
+
+char *shape_missile[] =
+{
+"WWWW",
+"WWWW",
+"WWWW",
+"WWWW",
+"WWWW",
+"WWWW",
+"WWWW",
+"WWWW",
+NULL
+};
+
 struct shape Shape[] =
 {
-  [0] = { std_colors, shape_ship },
-  [1] = { std_colors, shape_alien1u },
-  [2] = { std_colors, shape_alien1d },
-  [3] = { std_colors, shape_alien1l },
-  [4] = { std_colors, shape_alien1r },
-  [5] = { std_colors, shape_alien2u },
-  [6] = { std_colors, shape_alien2d },
-  [7] = { std_colors, shape_alien2l },
-  [8] = { std_colors, shape_alien2r },
-  [9] = { std_colors, shape_alien3u },
- [10] = { std_colors, shape_alien3d },
- [11] = { std_colors, shape_alien3l },
- [12] = { std_colors, shape_alien3r },
- [13] = { std_colors, shape_alien4u },
- [14] = { std_colors, shape_alien4d },
- [15] = { std_colors, shape_alien4l },
- [16] = { std_colors, shape_alien4r },
- [17] = { std_colors, shape_alien_radiate1 },
- [18] = { std_colors, shape_alien_radiate2 },
+  [0] = { std_colors, shape_alien1u },
+  [1] = { std_colors, shape_alien1d },
+  [2] = { std_colors, shape_alien1l },
+  [3] = { std_colors, shape_alien1r },
+  [4] = { std_colors, shape_alien2u },
+  [5] = { std_colors, shape_alien2d },
+  [6] = { std_colors, shape_alien2l },
+  [7] = { std_colors, shape_alien2r },
+  [8] = { std_colors, shape_alien3u },
+  [9] = { std_colors, shape_alien3d },
+ [10] = { std_colors, shape_alien3l },
+ [11] = { std_colors, shape_alien3r },
+ [12] = { std_colors, shape_alien4u },
+ [13] = { std_colors, shape_alien4d },
+ [14] = { std_colors, shape_alien4l },
+ [15] = { std_colors, shape_alien4r },
+ [16] = { std_colors, shape_alien_radiate1 },
+ [17] = { std_colors, shape_alien_radiate2 },
+ [18] = { std_colors, shape_bomb },
+ [19] = { std_colors, shape_missile },
+ [20] = { std_colors, shape_ship1 },
+ [21] = { std_colors, shape_ship2 },
 //  [99] = { NULL, NULL }
 };
 
